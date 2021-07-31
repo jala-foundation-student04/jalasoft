28d63da32706d20265cc89f387a6c183a5d5a9d1
Lordcreos <leonardosanchez4h@hotmail.com>
2021-07-31 11:25:08 -0500

    last name error solved

--- server/controllers/contactControllers.js
+++ server/controllers/contactControllers.js
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
-// importamos el modal
 import ContactSchema from '../model/contactSchema.js';
 
-
-// ruta get
 export const getContacts = async (req, res)  =>{
     
     try {
@@ -15,19 +12,16 @@ export const getContacts = async (req, res)  =>{
     }
 }
 
-// ruta post 
 export const createContact = async (req,res)=>{
-    // requerimos los valores necesarios
     const name = req.body.name;
-    const lastName = req.body.lasName;
+    const lastName = req.body.lastName;
     const email = req.body.email;
     const city = req.body.city;
     const country = req.body.country;
-    
-
     const newContact =  new ContactSchema ({
         name : name, lastName: lastName ,email : email, 
         country: country, city: city })
+   
     try {
         await newContact.save();
         res.json(newContact);
@@ -36,21 +30,19 @@ export const createContact = async (req,res)=>{
         console.log(error);
     } 
 }
- // ruta Patch 
+
 export const updateContact = async (req,res)=>{
-    // requerimos valores necesarios
     const name = req.body.name;
     const lastName = req.body.lasName;
     const email = req.body.email;
     const city = req.body.city;
-    // traemos el id por medio de la url
+    const country = req.body.country;
     const {id} = req.params;
 
-    const country = req.body.country;const updateContact =  new ContactSchema ({
+    const updateContact =  new ContactSchema ({
         name : name, lastName: lastName ,email : email, 
-         country: country, city: city  , _id: id })
+        country: country, city: city , _id: id })
     try {
-
         await ContactSchema.findByIdAndUpdate(id, updateContact, { new: true });
         console.log('updated', updateContact);
         
@@ -59,12 +51,10 @@ export const updateContact = async (req,res)=>{
         console.log (updateContact);
     }
 }
-// ruta delete
+
 export const  deleteContact = async (req, res) =>{
-    // traemos el id por el url
     const {id} = req.params;
-    
-    // condicional para validar si existe el id 
+
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`not valid id ${id}`);
 
     await ContactSchema.findByIdAndRemove(id);
