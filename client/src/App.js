eab9385fce3ecbb1dfbc110e9572c5b449b94a09
Lordcreos <leonardosanchez4h@hotmail.com>
2021-07-31 09:58:21 -0500

    algo salio mal

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,115 @@
-import MaterialTable from 'material-table';
-import {React, useEffect, useState} from 'react';
-import axios from 'axios';
-
-import {tableIcons} from './icons.js'
 
 import './App.css';
+import MaterialTable from 'material-table';
+import { React, useEffect, useState,  } from 'react';
+import {tableIcons} from './components/icons.js';
+import axios from 'axios';
 
-
-const columns= [
-  { title: "name", field: "name" },
-  { title: "lastName", field: "lastName" },
-  { title: "email", field: "email" },
-  { title: "city", field: "city" },
-  { title: "country", field: "country" }
-
-]
 
 const api = 'http://localhost:5000'
+
+
+
+
+
+const columns = [
+  {    title: 'Name',  field: 'name'},
+  {    title: 'lastName',    field: 'lastName',       },
+  {    title: 'Email',    field: 'email'       }  ,
+  {    title: 'City',    field: 'city'   },
+  {    title: 'Country',    field: 'Country'   }, 
+  
+]
+
 function App() {
+const [data,setData] = useState([]);
 
-  const [data,setData]= useState([]);
 
-const  getData = ()=>{
-  axios.get(api).then(response=>{
+
+
+ useEffect(() => {
+ 
+    getContact();
+    
+ },[]);
+
+ const getContact = async () => {
+  await axios.get(api).then(response =>{
     setData(response.data);
-  }
-    )
-}
-useEffect(() => {
-  getData(); 
-}, [])
+    
+  })
+}  
+
+ const create = async (newData)=>{
+   try {
+     await axios.post(api, newData);
+    console.log(newData);
+      
+   } catch (error) {
+     console.log(error);
+   }
+   
+ }
+
+ const deleted = async (deleteData)=>{
+   try {
+    axios.delete(`${api}/${deleteData._id}`)
+     console.log(deleteData._id);
+     
+   } catch (error) {
+     console.log (error);
+   }
+ } 
+
+ const update = async (updatedData) =>{
+   try {
+      axios.patch(`${api}/${updatedData._id}`, updatedData);
+      console.log(updatedData._id);
+   } catch (error) {
+     console.log(error)
+   }
+
+ }
+
+
+
 
   return (
     <MaterialTable
-      icons= {tableIcons}
-      columns= {columns}
-      title = "Contact Page Jalasoft Dev Tes"
-      data = {data}
-    />
+      columns = {columns}
+      data= {data}
+      title= "covid contact app"
+      icons={tableIcons}
+      localization={{ header:{  actions: "Acciones"    }, body: { editRow: { deleteText: '¿Estas seguro de querer eliminar?' }} }}
+      editable =  {{
+        onRowAdd : (newRow)=>  new Promise((resolve,reject)=>{
+          const newData =  newRow
+           create(newData);
+           window.location.reload();
+           resolve();
+        }),
+        onRowDelete: (selectedRow) => new Promise ((resolve,reject )=>{
+          const deleteData = selectedRow;
+          deleted(deleteData);
+          
+          window.location.reload();
+          resolve();
+        }),
+        onRowUpdate: (updatedRow, oldrow )=> new Promise((reject,resolve)=>{
+          const updatedData = updatedRow;
+          const oldData= oldrow;
+          update(updatedData);
+          window.location.reload();
+          resolve();
+        })
+      }}
 
+      options={{
+        actionsColumnIndex: -1 
+      }}
+
+    />
+    
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
- 
-    <App />,
   
+    <App />
+  ,
   document.getElementById('root')
 );
+
